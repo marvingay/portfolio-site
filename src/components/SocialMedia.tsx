b0abf9483100d38da41ed6d09f06c5807320f4d2
Marvin Gay <MarvinJGay@protonmail.com>
2020-06-27 23:39:32 -0400

    Added shadow to icons/rearrangement

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -7,10 +7,10 @@ import InstagramIcon from '../images/InstagramIcon.png';
 const SocialMedia: React.FC = () => {
   return (
     <div className="social">
-      <a href="https://www.github.com/marvingay"><img className="icon" src={GitHubIcon} alt="Github" /></a>
-      <a href="https://www.instagram.com/marvinbth"><img className="icon" src={InstagramIcon} alt="Instagram" /></a>
-      <a href="https://www.linkedin.com/in/marvinjgay"><img className="icon" src={LinkedInIcon} alt="LinkedIn" /></a>
-      <a href="https://www.twitter.com/MarvinJGay"><img className="icon" src={TwitterIcon} alt="Twitter" /></a>
+      <a href="https://www.github.com/marvingay"><img className="icon github" src={GitHubIcon} alt="Github" /></a>
+      <a href="https://www.instagram.com/marvinbth"><img className="icon instagram" src={InstagramIcon} alt="Instagram" /></a>
+      <a href="https://www.twitter.com/MarvinJGay"><img className="icon twitter" src={TwitterIcon} alt="Twitter" /></a>
+      <a href="https://www.linkedin.com/in/marvinjgay"><img className="icon linkedin" src={LinkedInIcon} alt="LinkedIn" /></a>
     </div>
 
   )
