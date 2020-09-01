7dcc180c2639cfa93620c2a618de8febfa28eab9
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-01 02:25:32 -0400

    Fix header text styles

--- src/components/Marvin.tsx
+++ src/components/Marvin.tsx
@@ -1,16 +1,19 @@
-import React from "react";
-import SocialMedia from "./SocialMedia";
+import React from "react"
+import SocialMedia from "./SocialMedia"
 
 const Marvin = () => {
   return (
     <div className="marvin">
-      <h1 id="name">Marvin Gay</h1>
-      <p id="job-title">Software Engineer
-       <p className="subtitle">(I play poker too!)</p>
-      </p>
+      <div className="text-container">
+        <h1 className="name">Marvin Gay</h1>
+        <p className="job-title">
+          Software Engineer
+          <p className="subtitle">(I play poker too!)</p>
+        </p>
+      </div>
       <SocialMedia />
     </div>
   )
 }
 
-export default Marvin;
+export default Marvin
