e05a402c04f47ffae37547dfc3d22e72d811d5ea
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-02 00:51:38 -0400

    Add Helmet to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
-import React from "react";
-import Header from "../components/Header";
-import Main from "../components/Main";
-import "../styles.scss";
+import React from "react"
+import Header from "../components/Header"
+import Main from "../components/Main"
+import "../styles.scss"
+import Helmet from "react-helmet"
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Portfolio | Marvin Gay</title>
+        <meta name="title" content="Portfolio | Marvin Gay" />
+        <meta
+          name="description"
+          content="The portfolio website for Marvin Gay, a software engineer, poker player, and sports enthusiast."
+        />
+      </Helmet>
       <Header />
       <Main />
     </>
   )
 }
 
-export default Home;
+export default Home
