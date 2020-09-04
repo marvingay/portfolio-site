9838a9b1ca1ac40b3224d9efb1e5142758e57a95
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-04 18:47:27 -0400

    Remove IG Icon

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import GitHubIcon from '../images/GitHubIcon.png';
-import LinkedInIcon from '../images/LinkedInIcon.png';
-import TwitterIcon from '../images/TwitterIcon.png';
-import InstagramIcon from '../images/InstagramIcon.png';
+import React from "react"
+import GitHubIcon from "../images/GitHubIcon.png"
+import LinkedInIcon from "../images/LinkedInIcon.png"
+import TwitterIcon from "../images/TwitterIcon.png"
 
 const SocialMedia: React.FC = () => {
   return (
     <div className="social">
-      <a href="https://www.github.com/marvingay"><img className="icon github" src={GitHubIcon} alt="Github" /></a>
-      <a href="https://www.instagram.com/marvinbth"><img className="icon instagram" src={InstagramIcon} alt="Instagram" /></a>
-      <a href="https://www.twitter.com/MarvinJGay"><img className="icon twitter" src={TwitterIcon} alt="Twitter" /></a>
-      <a href="https://www.linkedin.com/in/marvinjgay"><img className="icon linkedin" src={LinkedInIcon} alt="LinkedIn" /></a>
+      <a href="https://www.github.com/marvingay">
+        <img className="icon github" src={GitHubIcon} alt="Github" />
+      </a>
+      <a href="https://www.twitter.com/MarvinJGay">
+        <img className="icon twitter" src={TwitterIcon} alt="Twitter" />
+      </a>
+      <a href="https://www.linkedin.com/in/marvinjgay">
+        <img className="icon linkedin" src={LinkedInIcon} alt="LinkedIn" />
+      </a>
     </div>
-
   )
 }
 
-export default SocialMedia;
+export default SocialMedia
