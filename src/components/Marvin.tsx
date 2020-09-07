1959e4151862fd028c587326f01ea335d6217075
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-07 01:17:11 -0400

    Move job title/subtitle to own container; fix margins

--- src/components/Marvin.tsx
+++ src/components/Marvin.tsx
@@ -6,10 +6,10 @@ const Marvin = () => {
     <div className="marvin">
       <div className="text-container">
         <h1 className="name">Marvin Gay</h1>
-        <p className="job-title">
-          Software Engineer
+        <div>
+          <p className="job-title">Software Engineer</p>
           <p className="subtitle">(I love sports analysis too!)</p>
-        </p>
+        </div>
       </div>
       <SocialMedia />
     </div>
