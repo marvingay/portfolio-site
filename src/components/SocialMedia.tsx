d4547b25d97eea298f74d61e3b4f3562c4d1bd2b
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-21 11:56:08 -0400

    Added noopener rel to social media links

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -6,13 +6,13 @@ import TwitterIcon from "../images/TwitterIcon.png"
 const SocialMedia: React.FC = () => {
   return (
     <div className="social">
-      <a href="https://www.github.com/marvingay">
+      <a href="https://www.github.com/marvingay" rel="noopener">
         <img className="icon github" src={GitHubIcon} alt="Github" />
       </a>
-      <a href="https://www.twitter.com/MarvinJGay">
+      <a href="https://www.twitter.com/MarvinJGay" rel="noopener">
         <img className="icon twitter" src={TwitterIcon} alt="Twitter" />
       </a>
-      <a href="https://www.linkedin.com/in/marvinjgay">
+      <a href="https://www.linkedin.com/in/marvinjgay" rel="noopener">
         <img className="icon linkedin" src={LinkedInIcon} alt="LinkedIn" />
       </a>
     </div>
